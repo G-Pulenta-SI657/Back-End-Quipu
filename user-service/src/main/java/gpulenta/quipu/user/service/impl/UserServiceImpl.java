3139d00759269941a3f9669945f20f1bc78f87a3
Aldo Medina <u202018286@upc.edu.pe>
2023-10-20 19:54:49 -0500

    Add: UserServiceImpl service implementation file was added.

---
+++ user-service/src/main/java/gpulenta/quipu/user/service/impl/UserServiceImpl.java
@@ -0,0 +1,60 @@
+package gpulenta.quipu.user.service.impl;
+
+import gpulenta.quipu.user.model.User;
+import gpulenta.quipu.user.repository.UserRepository;
+import gpulenta.quipu.user.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
+
+public class UserServiceImpl implements UserService {
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public List<User> findAll() {
+        return userRepository.findAll();
+    }
+
+    @Override
+    public User createUser(User user) {
+        return userRepository.save(user);
+    }
+
+    @Override
+    public User getUserByUsername(String username) {
+        return userRepository.findByUsername(username).orElse(null);
+    }
+
+    @Override
+    public User getUserByEmailAndPassword(String email, String password) {
+        return userRepository.findByEmailAndPassword(email, password).orElse(null);
+    }
+
+    @Override
+    public User updateUser(User user) {
+        User userToUpdate = userRepository.findById(user.getId()).orElse(null);
+        if (userToUpdate != null) {
+            userToUpdate.setUsername(user.getUsername());
+            userToUpdate.setEmail(user.getEmail());
+            userToUpdate.setPassword(user.getPassword());
+            userToUpdate.setName(user.getName());
+            userToUpdate.setLastname(user.getLastname());
+            userToUpdate.setAddress(user.getAddress());
+            userToUpdate.setPhone(user.getPhone());
+            return userRepository.save(userToUpdate);
+        } else {
+            return null;
+        }
+    }
+
+    @Override
+    public void deleteUser(Long id) {
+        userRepository.deleteById(id);
+    }
+
+    @Override
+    public User findById(Long id) {
+        return userRepository.findById(id).orElse(null);
+    }
+}
