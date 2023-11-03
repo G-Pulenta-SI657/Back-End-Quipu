7021467833a3fd46c87bf107ca298d6118f6e32e
DiegoMerinoMas <u202111473@upc.edu.pe>
2023-11-02 22:37:54 -0500

    add: Offer controller and message controller

---
+++ offer-service/src/test/java/gpulenta/quipu/offer/OfferServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package gpulenta.quipu.offer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OfferServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
